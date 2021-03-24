0bc9477dbf8452832faa899eaaaeee5332c77471
Axel <axosar@gmail.com>
2021-03-23 20:11:10 -0600

    Edit contact

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,10 +1,18 @@
-import { ADD_CONTACT, DELETE_CONTACT, GET_CONTACTS } from './types';
+import { ADD_CONTACT, DELETE_CONTACT, GET_CONTACTS, GET_CONTACT, UPDATE_CONTACT } from './types';
 import axios from 'axios';
 
 export const getContacts = () => async dispatch => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/users')
   dispatch({
     type: GET_CONTACTS,
+    payload: res.data
+  })
+}
+
+export const getContact = (id) => async dispatch => {
+  const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  dispatch({
+    type: GET_CONTACT,
     payload: res.data
   })
 }
