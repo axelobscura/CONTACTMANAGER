0ea39fc97f333588dc52234856d50ac522780a1c
Axel <axosar@gmail.com>
2021-03-24 20:15:07 -0600

    Update contact

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getContact } from '../../actions/contactActions';
+import { getContact, updateContact } from '../../actions/contactActions';
 
 class EditContact extends Component {
   state = {
@@ -26,7 +26,7 @@ class EditContact extends Component {
     this.props.getContact(id)
   }
 
-  onSubmit = (e) => {
+  onSubmit = e => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
@@ -47,15 +47,17 @@ class EditContact extends Component {
       return;
     }
 
+    const { id } = this.props.match.params;
+
     const updContact = {
+      id,
       name,
       email,
       phone
     };
 
-    const { id } = this.props.match.params;
-
     //// UPDATE CONTACT ////
+    this.props.updContact(updContact)
 
     // Clear State
     this.setState({
@@ -124,4 +126,4 @@ const mapStateToProps = state => ({
   contact: state.contact.contact
 });
 
-export default connect(mapStateToProps, { getContact })(EditContact);
+export default connect(mapStateToProps, { getContact, updateContact })(EditContact);
